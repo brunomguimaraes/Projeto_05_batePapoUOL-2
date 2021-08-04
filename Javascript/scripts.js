a4b8aa46daa8fcdb9c02d7dc2a7f4be6351a3716
Carlos <carlosedufpaiva@poli.ufrj.br>
2021-08-04 10:49:21 -0300

    Implemented feature: When the website is loaded, it downloads all the messages from the given server and plots it on its main section. Styles for each message are yet to be implemented.

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -6,3 +6,43 @@ function sidebarSwitch () {
     sidebar.classList.toggle("active");
     darkScreen.classList.toggle("active");
 }
+
+function searchChatRoomMessages () {
+    const messagesPromise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
+    messagesPromise.then(loadChatRoomMessages);
+}
+
+function loadChatRoomMessages (messagesAnswer) {
+    const messages = messagesAnswer.data
+    console.log(messages);
+    const messagesSection = document.querySelector(".messages");
+
+    for (let i = 0 ; i < messages.length ; i++) {
+        let activeObject = messages[i]
+        let messageMiddleSection = ``
+        if (activeObject.type === "status") {
+            messageMiddleSection = `<span class = "contact">${activeObject.from} </span>`;
+        } else if (activeObject.type === "private_message") {
+            messageMiddleSection = `
+            <span class = "contact">${activeObject.from} </span>
+            reservadamente para 
+            <span class = "contact">${activeObject.to}: </span>`
+        } else if (activeObject.type === "message") {
+            messageMiddleSection = `
+            <span class = "contact">${activeObject.from} </span>
+            para 
+            <span class = "contact">${activeObject.to}: </span>`
+        }
+        
+        let completeMessage = `
+        <li>
+            <span class = "time">(${activeObject.time})</span>
+            ${messageMiddleSection}
+            ${activeObject.text}
+        </li>`
+
+        messagesSection.innerHTML += completeMessage
+    }
+}
+
+searchChatRoomMessages ()
