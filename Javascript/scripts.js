5766c58bf36af2c695d7fa213e5e6de8e5f51b8f
Carlos <carlosedufpaiva@poli.ufrj.br>
2021-08-04 09:52:36 -0300

    Implemented features: 1 - Final Layout completed. / 2 - when the top right corner button is clicked, it shows a sidebar with contacts and types of messages, which for now are not taken from the server. Next to the sidebar, a dark screen appears on top of the remaining chat / 3 - When the dark screen is clicked, the sidebar and the dark screen are removed and the chat reappears completely.

---
+++ Javascript/scripts.js
@@ -0,0 +1,8 @@
+
+
+function sidebarSwitch () {
+    const sidebar = document.querySelector(".sidebar");
+    const darkScreen = document.querySelector(".dark-screen");
+    sidebar.classList.toggle("active");
+    darkScreen.classList.toggle("active");
+}
